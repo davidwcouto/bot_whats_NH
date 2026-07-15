6dfacd97333a586c03ace400a5f709139522a409
davidwcouto <david.kouto@hotmail.com>
2026-07-15 00:51:38 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ cloudinary.config({
 let allowedContacts = [];
 try {
   const contactsData = fs.readFileSync("allowed.txt", "utf8");
-  // Divide o conteúdo em linhas, remove espaços e filtra linhas vazias
+  // Divide o conteúdo em linhas, remove espaços e filtra linhas vaziass
   allowedContacts = contactsData
     .split("\n")
     .map(line => line.trim())
