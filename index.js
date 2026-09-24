bc523db4098cac08782e2298656702cc8115cba9
davidwcouto <david.kouto@hotmail.com>
2026-09-24 12:21:32 -0300

    Atualizando o bot

--- index.js
+++ index.js
@@ -1088,6 +1088,15 @@ client.on("message", async (message) => {
 
 					const texto = String(resultado?.data?.text || "")
 						.toLowerCase();
+						
+					// Ignorar imagens de entrega
+					if (
+						texto.includes("entrega moto") ||
+						texto.includes("entrega carro")
+					) {
+						console.log("Imagem ignorada: imagem de entrega.");
+						return;
+					}
 
 					const palavrasChave = [
 						"pix",
